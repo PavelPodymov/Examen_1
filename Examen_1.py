e298b72e475b282ac907555b3baf764f881654d4
PavelPodymov <poll.ppbpower@gmail.com>
2022-11-09 11:12:33 +0300

    Wrote a solution

--- Examen_1.py
+++ Examen_1.py
@@ -1,1 +1,8 @@
-print("Hello World")
+# array = ["hello", "2", "world", ":-)"]  # ["2", ":-)"]
+array = ["1234", "1567", "-2", "computer science"]    # ["-2"]
+# array = ["Russia", "Denmark", "Kazan"]  # []
+
+new_array = [i for i in array if len(i) <= 3]
+print(new_array)
+
+
